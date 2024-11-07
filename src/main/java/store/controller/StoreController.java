ed31d9915af0ae775fc119e9977569a2b47aed66
sunwon <jsw5913@naver.com>
2024-11-08 04:27:50 +0900

    remove: StoreController에서 StoreService 삭제

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,17 +1,14 @@
 package store.controller;
 
-import store.model.StoreService;
 import store.view.InputView;
 import store.view.OutptView;
 
 public class StoreController {
 
-    private final StoreService storeService;
     private final InputView inputView;
     private final OutptView outptView;
 
-    public StoreController(StoreService storeService, InputView inputView, OutptView outptView) {
-        this.storeService = storeService;
+    public StoreController(InputView inputView, OutptView outptView) {
         this.inputView = inputView;
         this.outptView = outptView;
     }
