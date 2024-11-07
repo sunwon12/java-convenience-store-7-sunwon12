a9a2719c374a394c1b557d6a60e6628c0f32527b
sunwon <jsw5913@naver.com>
2024-11-08 04:29:08 +0900

    remove: controller에서 service 삭제

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,14 +1,13 @@
 package store;
 
 import store.controller.StoreController;
-import store.model.StoreService;
 import store.view.InputView;
 import store.view.OutptView;
 import store.view.SimpleValidator;
 
 public class Application {
     public static void main(String[] args) {
-        StoreController storeController = new StoreController(new StoreService(), new InputView(new SimpleValidator()),
+        StoreController storeController = new StoreController(new InputView(new SimpleValidator()),
                 new OutptView());
         storeController.process();
     }
