b2051602fba451c7f079f270af64f2d3efdafd8d
sunwon <jsw5913@naver.com>
2024-11-08 03:52:14 +0900

    remove: Stock 물품 초기화 로직 삭제

--- src/main/java/store/model/product/Stock.java
+++ src/main/java/store/model/product/Stock.java
@@ -13,53 +13,10 @@ public class Stock {
         this.products = new ArrayList<>();
     }
 
-
     /**
      * 파일에서 물건 목록을 가져와 창고에 넣어준다
      */
     public void initializeStock() {
         List<StockProductDto> stockProductDtos = new CustomFileReader().loadProducts();
-
-        List<Product> products = new ArrayList<>();
-        for (StockProductDto dto : stockProductDtos) {
-            Product product = new Product(
-                    dto.name(), dto.price(), dto.count(), dto.promotionType());
-            products.add(product);
-        }
-        List<Product> productsWithZeroNomal = addZeroNormalProducts(products);
-        this.products = productsWithZeroNomal;
-    }
-
-    private List<Product> addZeroNormalProducts(List<Product> originalProducts) {
-        List<Product> result = new ArrayList<>(originalProducts);
-
-        for (int i = 0; i < originalProducts.size(); i++) {
-            Product current = originalProducts.get(i);
-            if (shouldAddNormal(current, originalProducts)) {
-                result.add(i + 1, createNormalProduct(current.getName(), current.getPrice()));
-            }
-        }
-
-        return result;
-    }
-
-    private boolean shouldAddNormal(Product product, List<Product> allProducts) {
-        return product.isPromotion()
-                && !hasNormal(product.getName(), allProducts);
-    }
-
-    private boolean hasNormal(String name, List<Product> products) {
-        return products.stream()
-                .anyMatch(sp -> sp.getName().equals(name)
-                        && sp.getPromotionType() == PromotionType.NONE);
-    }
-
-    private Product createNormalProduct(String name, int price) {
-        return new Product(
-                name,
-                price,
-                0,
-                PromotionType.NONE
-        );
     }
 }
